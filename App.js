c1b8e8e847c5a46c20bbaaf8a8f9b2a13722fd86
kyicho <kyicho@outlook.com>
2022-09-30 23:13:54 -0400

    registration : create registration page and fixed app.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import Launch from './src/pages/launch/Launch'
+import Registration from './src/pages/registration/Registration'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
@@ -11,8 +12,9 @@ export default function App () {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Registration" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Registration" component={ Launch } />
+        <Stack.Navigator initialRouteName="Launch" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Launch" component={ Launch } />
+          <Stack.Screen name="Registration" component={ Registration } />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
